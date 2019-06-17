48503c0998e809328d8380c5b6a26624dc795956
Ayush Subedi <ayush.subedi@gmail.com>
2019-06-17 09:41:40 +0545

    Add api

--- trovenow_url_parser/api_routes.py
+++ trovenow_url_parser/api_routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, request, jsonify
 from trovenow_url_parser import application
+from trovenow_url_parser.models import ContentReader
 
-
-# @application.route('/')
-# @application.route('/index')
-# def index():
-#     return "hello world"
+@application.route('/api/get_content', methods=['GET'])
+def get_content():
+    url = request.args.get('url', None)
+    result = ContentReader.get_content(url)
+    return jsonify(result)
