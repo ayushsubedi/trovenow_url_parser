5bbe6122945101e3a06c823df475d517bd348c58
Ayush Subedi <ayush.subedi@gmail.com>
2019-06-26 18:28:04 +0545

    Remove npm for first version

--- trovenow_url_parser/models.py
+++ trovenow_url_parser/models.py
@@ -13,19 +13,19 @@ class ContentReader:
                 extension = mimetypes.guess_extension(content_type)
                 if ((extension is not None) and extension != ".htm"):
                     return {'code_content': 204, 'title': external_sites_url.split('/')[-1], 'error:': "content is not html"}
-                article = Article(external_sites_url, keep_article_html=True)
+                article = Article(external_sites_url, keep_article_html=False)
                 article.download()
                 article.parse()
-                article.nlp()
-                new_keywords = []
-                for word in article.keywords:
-                    if (len(word) > 5):
-                        new_keywords.append(word)
+                # article.nlp()
+                # new_keywords = []
+                # for word in article.keywords:
+                #     if (len(word) > 5):
+                #         new_keywords.append(word)
                 try:
                     _type = article.meta_data.get("og").get("type")
                 except:
                     _type = None
-                return {'code_content': 200, 'title': article.title, 'movies': article.movies, 'description': article.meta_description, 'type': _type, 'text': article.article_html, 'top_image': article.top_image, 'authors': article.authors, 'publish_date': article.publish_date, 'summary': article.summary, 'keywords': new_keywords}
+                return {'code_content': 200, 'title': article.title, 'movies': article.movies, 'description': article.meta_description, 'type': _type, 'text': "", 'top_image': article.top_image, 'authors': article.authors, 'publish_date': article.publish_date, 'summary': "", 'keywords': new_keywords}
             except Exception as e:
                 return {'code_content': 500, 'error': str(e)}
         return {'code_content': 404, 'msg': 'malinformed URL'}
