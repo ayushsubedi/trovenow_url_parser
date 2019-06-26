00226cfdff785f8e238a5f3d4010c277576e65ea
Ayush Subedi <ayush.subedi@gmail.com>
2019-06-26 21:27:55 +0545

    Add Post request to api

--- trovenow_url_parser/api_routes.py
+++ trovenow_url_parser/api_routes.py
@@ -2,8 +2,11 @@ from flask import render_template, request, jsonify
 from trovenow_url_parser import application
 from trovenow_url_parser.models import ContentReader
 
-@application.route('/api/get_content', methods=['GET'])
+@application.route('/api/get_content', methods=['GET', 'POST'])
 def get_content():
-    url = request.args.get('url', None)
+    if request.method == 'POST':
+        url = request.form['url']
+    else:
+        url = request.args.get('url', None)
     result = ContentReader.get_content(url)
     return jsonify(result)
